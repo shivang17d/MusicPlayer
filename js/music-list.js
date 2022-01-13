cf77afbc4e481f539c67ee76aca82e2784a28e21
Shivang Gupta <shivangguptaxd@gmail.com>
2022-01-13 22:49:20 +0530

    initial

---
+++ js/music-list.js
@@ -0,0 +1,174 @@
+let allMusic = [{
+        name: "Aao Na",
+        artist: "Udit-Sadhana",
+        src: "music-1"
+    },
+    {
+
+        name: "Tumhain Jo Meine Dekha ",
+        artist: "Abhijeet-Shreya",
+        src: "music-2"
+    },
+    {
+        name: "Main Hoon Na",
+        artist: "Sonu-Shreya",
+        src: "music-3"
+    },
+    {
+        name: "Guzarish",
+        artist: "Javed Ali",
+        src: "music-4"
+    },
+    {
+        name: "Kaise Mujhe Tum Mil Gayi",
+        artist: "Shreya-Benny",
+        src: "music-5"
+    },
+    {
+        name: "Chaaha Hai Tujhko",
+        artist: "Udit-Anuradha",
+        src: "music-6"
+    },
+    {
+        name: "Nasha Ye Pyar ka",
+        artist: "Udit Narayan",
+        src: "music-7"
+    },
+    {
+        name: "Tera Hone Laga Hoon",
+        artist: "Atif Aslam",
+        src: "music-8"
+    },
+    {
+        name: "Pelha Nasha",
+        artist: "Udit-Sadhana",
+        src: "music-9"
+    },
+    {
+        name: "Jo Bheji Thi Dua",
+        artist: "Maham Waqar",
+        src: "music-10"
+    },
+    {
+        name: "Kal Ho Na Ho",
+        artist: "Sonu Nigam",
+        src: "music-11"
+    },
+    {
+        name: "Hasi",
+        artist: "Shreya Ghoshal",
+        src: "music-12"
+    },
+    {
+        name: "Sapna Jahan",
+        artist: "Sonu Nigam",
+        src: "music-13"
+    },
+    {
+        name: "Dekho Na",
+        artist: "Sonu-Sunidhi",
+        src: "music-14"
+    },
+    {
+        name: "Chand Sifarish",
+        artist: "Shaan",
+        src: "music-15"
+    },
+    {
+        name: "Saans",
+        artist: "Shreya Ghoshal",
+        src: "music-16"
+    },
+    {
+        name: "Main Agar Kahoon",
+        artist: "Sonu-Shreya",
+        src: "music-17"
+    },
+    {
+        name: "Meri Aashiqui",
+        artist: "Arijit-Palak",
+        src: "music-18"
+    },
+    {
+        name: "Sunn Raha Hai Na",
+        artist: "Shreya Ghoshal",
+        src: "music-19"
+    },
+    {
+        name: "Chahun Main Ya Naa",
+        artist: "Arijit-Palak",
+        src: "music-20"
+    },
+    {
+        name: "Kissa Hum Likhenge",
+        artist: "Sreekumar-Anuradha",
+        src: "music-21"
+    },
+    {
+        name: "Tere Liye",
+        artist: "Lata - Roopkumar",
+        src: "music-22"
+    },
+    {
+        name: "Jo Tum Na Ho",
+        artist: "Arijit Singh",
+        src: "music-23"
+    },
+    {
+        name: "Aye Khuda",
+        artist: "Salim Merchant",
+        src: "music-24"
+    },
+    {
+        name: "Koi Mil Gaya",
+        artist: "Udit-Alka-Kavita",
+        src: "music-25"
+    },
+    {
+        name: "Kuch Kuch Hota Hai",
+        artist: "Udit-Alka",
+        src: "music-26"
+    },
+    {
+        name: "Ladki Badi Anjani Hai ",
+        artist: "Kumar Sanu - Alka",
+        src: "music-27"
+    },
+    {
+        name: "Yeh Ladka Hai Deewana",
+        artist: "Udit-Alka",
+        src: "music-28"
+    },
+    {
+        name: "Humko Humise Chura Lo",
+        artist: "Lata Mangeshkar-Udit",
+        src: "music-29"
+    },
+    {
+        name: "Suraj Hua Madham",
+        artist: "Sonu-Alka",
+        src: "music-30"
+    },
+    {
+        name: "Dil Se Re",
+        artist: "A.R. Rahman",
+        src: "music-31"
+    },
+    {
+        name: "Hai Mera Dil",
+        artist: "Alka-Udit",
+        src: "music-32"
+    },
+    {
+        name: "Tumhi Dekho Na",
+        artist: "Sonu-Alka",
+        src: "music-33"
+    },
+    {
+        name: "Dil Ko Karaar Aaya",
+        artist: "Rajat-Neha-Yasser",
+        src: "music-34"
+    },
+
+
+];
